5f308197ab26ecb0314ab519fed09e72340524be
xtcube <palmarthc@gmail.com>
2022-09-23 15:55:08 -0400

    Create Search.jsx

---
+++ Proyecto-react/my-project/src/pages/Search.jsx
@@ -0,0 +1,12 @@
+import SearchComponent from '../component/SearchComponent';
+
+function Search() {
+    return (
+      <div className="container-fluid">
+      <h2 className="text-center text-2xl ">Buscador Conectando una Api</h2>
+       <SearchComponent />     
+     </div>
+    );
+  }
+  
+  export default Search;
