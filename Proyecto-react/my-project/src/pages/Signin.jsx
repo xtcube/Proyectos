61a40582a1afe38d156e7f717f42a3a79a1e4bc0
xtcube <palmarthc@gmail.com>
2022-09-23 15:55:11 -0400

    Create Signin.jsx

---
+++ Proyecto-react/my-project/src/pages/Signin.jsx
@@ -0,0 +1,11 @@
+function Signin() {
+    return (
+      <div>
+   <h1 className="text-3xl font-bold underline">
+        Sign-In!
+      </h1>
+      </div>
+    );
+  }
+  
+  export default Signin;
