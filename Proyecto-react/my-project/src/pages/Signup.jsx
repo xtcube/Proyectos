a4f377de3ca5b31e2753b427fec816e38445a15b
xtcube <palmarthc@gmail.com>
2022-09-23 15:55:14 -0400

    Create Signup.jsx

---
+++ Proyecto-react/my-project/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+function Signup() {
+    return (
+      <div>
+   <h1 className="text-3xl font-bold underline">
+        Sign-up!
+      </h1>
+      </div>
+    );
+  }
+  
+  export default Signup;
